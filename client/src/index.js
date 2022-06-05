d173c26baac8d23e85dbc2bc77452c49a3358f63
Benjamin Roth <benjaminsavanne@gmail.com>
2022-06-04 20:55:23 -0500

    changed around file structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
 import './styles/index.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Landing from './pages/Landing'
+import {BrowserRouter} from 'react-router-dom'
+import Routings from './components/Routings'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Landing />} />
-    <Route path="/home" element={<Home />} />
-  </Routes>
+<BrowserRouter>
+  <Routings />
 </BrowserRouter>
 );
 
